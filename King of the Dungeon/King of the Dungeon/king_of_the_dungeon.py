10205c32620f7ebac15be420ec0913fb372728e1
Eneko Pinzolas Murua <kelossus@localhost.localdomain>
2015-08-22 09:52:25 +0200

    entered the correct values in the GUILayer .pngs, however I am unable to execute the game due to Visual Studio compatibility issues (maybe?)

--- King of the Dungeon/King of the Dungeon/king_of_the_dungeon.py
+++ King of the Dungeon/King of the Dungeon/king_of_the_dungeon.py
@@ -25,41 +25,50 @@ class GUILayer(Layer):
         
 
         ############   goblin house    ###################
-        house_goblin = ImageMenuItem("grossini.png", None)
-        house_goblin.selected_effect = selected_goblin()
+        house_goblin = ImageMenuItem("resources/goblin_quarters.png", None)
         houses.append(house_goblin)
         positions.append((100,60))
 
         ############  hobgoblin house  ###################
 
-        house_hobgoblin = ImageMenuItem("grossini.png", None)
+        house_hobgoblin = ImageMenuItem("resources/hobgoblin_quarters.png", None)
         houses.append(house_hobgoblin)
         positions.append((200,60))
 
         ############     orc  house    ###################
 
-        house_orc = ImageMenuItem("grossini.png", None)
+        house_orc = ImageMenuItem("resources/orc_quarters.png", None)
         houses.append(house_orc)
         positions.append((300,60))
 
-        ############  madgnome house   ###################
+        ############  madgnome house   ##################
 
-        house_madgnome = ImageMenuItem("grossini.png", None)
+        house_madgnome = ImageMenuItem("resources/madgnome_quarters.png", None)
         houses.append(house_madgnome)
         positions.append((400,60))
 
         ############ necromancer house ###################
 
-        house_necromancer = ImageMenuItem("grossini.png", None)
+        house_necromancer = ImageMenuItem("resources/necromancer_quarters.png", None)
         houses.append(house_necromancer)
         positions.append((500,60))
 
         ############    create menu    ###################
+
         self.create_menu(   houses,
                             activated_effect=self.shake(),
                             selected_effect=None,
                             unselected_effect=None,
                             layout_strategy=fixedPositionMenuLayout(positions))
+
+        ############    animations     ###################
+
+        house_goblin.selected_effect = selected_goblin()
+        house_hobgoblin.selected_effect = selected_hobgoblin()
+        house_orc.selected_effect = selected_orc()
+        house_madgnome.selected_effect = selected_madgnome()
+        house_necromancer.selected_effect = selected_necromancer()
+
     def shake(self):
         """Predefined action that performs a slight rotation and then goes back to the original rotation
         position.
@@ -73,6 +82,18 @@ class GUILayer(Layer):
 
     def selected_goblin(self):
         self.HUD_ref.text.element.text = "this is a very dangerous \n hack"
+
+    def selected_hobgoblin(self):
+        self.HUD_ref.text.element.text = "this is a hobvery dangerous \n hack"
+
+    def selected_orc(self):
+        self.HUD_ref.text.element.text = "this is a orcstremely dangerous \n hack"
+
+    def selected_madgnome(self):
+        self.HUD_ref.text.element.text = "dude, it even says mad \n hack"
+
+    def selected_necromancer(self):
+        self.HUD_ref.text.element.text = "corpse and shit \n\n backstab"
 
 class DynamicLayer(Layer):
     pass
